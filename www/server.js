af2ee1ca50a05794fb1427bd6177ea019e50a980
swastiknaikwad <swastik.naikwad@gmail.com>
2020-06-13 16:00:51 +0530

    initial setup

---
+++ www/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const Joi = require('joi');
+
+const db = require("./db");
+const collection = "todo";
+const app = express();
+
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app.listen(3000,()=>{
+            console.log('connected to database, app listening on port 3000');
+        });
+    }
+});
